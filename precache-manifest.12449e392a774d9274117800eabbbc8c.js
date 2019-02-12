466431061574eb2570c8c228ac35bfc0f11e1ed2
JacobToftgaard <42829337+JacobToftgaard@users.noreply.github.com>
2019-02-12 12:42:39 +0100

    First commit

---
+++ precache-manifest.12449e392a774d9274117800eabbbc8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2a13cc5fbb8c18d24c39",
+    "url": "/static/js/main.2a13cc5f.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "2a13cc5fbb8c18d24c39",
+    "url": "/static/css/main.1951e82e.chunk.css"
+  },
+  {
+    "revision": "616cf1ecb9de0bf798a06b78ad3cfc83",
+    "url": "/index.html"
+  }
+];
